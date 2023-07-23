f7008dc153a422d12394fb739c8fcff2eb1ecfe2
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-24 00:41:54 +0300

    add Bio component

---
+++ components/Bio.js
@@ -0,0 +1,34 @@
+const Bio = () => {
+  return (
+    <div className="bio-container">
+      <div className="avatar">
+        <img src="bg.jpg"></img>
+      </div>
+      <div className="bio">
+        <h2>Yousif Abozid - Full Stack Software Engineer | Novelist | Entrepreneur</h2>
+        <p>
+          Greetings! I take pride in being a skilled and dedicated Full Stack Software Engineer, driven by my enthusiasm
+          for crafting cutting-edge and intuitive web applications. My expertise spans across both Front-end and
+          Back-end development, allowing me to deliver well-rounded solutions.
+        </p>
+        <p>
+          I specialize in developing robust and scalable solutions that deliver seamless user experiences. My core
+          expertise lies in leveraging the power of javascript through it's libraries and frameworks like react.js and
+          specially next.js which allowing me to create dynamic and responsive web applications.
+        </p>
+        <p>
+          In addition to my technical prowess, I am also an accomplished Novelist, capable of weaving captivating
+          stories that resonate with readers. On top of that, I'm an Entrepreneur, and I am honored to be the Founder &
+          CEO of Gig Bud, a venture that fuels my passion for innovation and business leadership.
+        </p>
+        <p>
+          I'm constantly seeking opportunities to push boundaries and create meaningful impact through my work. Whether
+          it's writing code or crafting narratives, I strive for excellence, making a positive difference in the world
+          around me.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Bio
