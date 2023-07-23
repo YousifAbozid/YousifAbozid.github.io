2790fc2d4868bf4dad5edf094274c44ff3e8d0ec
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-24 00:43:06 +0300

    add bio section and change styles

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Bio from '@/components/Bio'
 import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <div className="container">
       <Header />
+      <Bio />
     </div>
   )
 }
