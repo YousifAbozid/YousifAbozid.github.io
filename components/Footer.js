0925d89657c340d0ef298cb8ef47461240ea57cc
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-24 14:08:01 +0300

    add Footer component

---
+++ components/Footer.js
@@ -0,0 +1,28 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="logo">
+        <img src="coding.svg" alt="Coding" />
+      </div>
+      <div className="middle">
+        <div className="text">
+          Made with ♥️ by <Link href="https://github.com/YousifAbozid">Me</Link> Source Code on
+        </div>
+        <div className="github">
+          <Link href="https://github.com/YousifAbozid/YousifAbozid.github.io">
+            <img src="github.svg" alt="GitHub" />
+          </Link>
+        </div>
+      </div>
+      <div className="scroll">
+        <img src="up.svg" alt="Go Up" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} />
+      </div>
+    </div>
+  )
+}
+
+export default Footer
