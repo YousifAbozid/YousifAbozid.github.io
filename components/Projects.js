e24f7d7561d4d8a1ddd25598ec0708bbc42845b6
Yousif Abozid <yousif.abozid@yahoo.com>
2023-08-27 18:30:18 +0300

    add projects array with dummy data

---
+++ components/Projects.js
@@ -0,0 +1,18 @@
+const ProjectsArray = [
+  {
+    name: 'Url Shrinker',
+    description: 'A simple URL Shrinker App build with MERN Stack',
+    tools: ['React', 'Node', 'Express', 'MongoDB'],
+    live: 'https://urlshrinker.netlify.app/',
+    source: 'https://github.com/YousifAbozid/url-shrinker',
+  },
+  {
+    name: 'ecommerce',
+    description: 'A simple URL Shrinker App build with MERN Stack',
+    tools: ['React', 'Node', 'Express', 'MongoDB'],
+    live: '',
+    source: 'https://github.com/YousifAbozid/url-shrinker',
+  },
+]
+
+export default ProjectsArray
