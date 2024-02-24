8847300675bb00d51f903c35513e2b4dca28000a
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-24 21:06:58 +0200

    Update BioSection.js

--- components/home/BioSection.js
+++ components/home/BioSection.js
@@ -15,7 +15,7 @@ const BioSection = () => {
           height={0}
           priority={true}
           src={
-            'https://res.cloudinary.com/fswd/image/upload/v1693155786/Khalifa_446_-min_wavfue.jpg'
+            'https://res.cloudinary.com/octopus-labs/image/upload/v1693155786/yousif-mini.jpg'
           }></Image>
       </div>
       <div className="bio">
