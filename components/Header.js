4054dd3af30fe9371aa150a4f0da98411417aa9c
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-29 13:06:57 +0200

    Refactor header component and update CSS

--- components/Header.js
+++ components/Header.js
@@ -8,43 +8,43 @@ import React, { useState } from 'react'
  * @return {JSX.Element} The header component.
  */
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false)
+	const [showMenu, setShowMenu] = useState(false)
 
-  /**
-   * Toggles the menu display.
-   *
-   * @return {void} - No return value
-   */
-  const toggleMenu = () => {
-    setShowMenu((prevState) => !prevState)
-  }
-  return (
-    <div className="header">
-      <Link href="/" className="logo">
-        <img src="logo.png"></img>
-      </Link>
-      <div className="text">
+	/**
+	 * Toggles the menu display.
+	 *
+	 * @return {void} - No return value
+	 */
+	const toggleMenu = () => {
+		setShowMenu((prevState) => !prevState)
+	}
+	return (
+		<div className='header'>
+			<Link href='/' className='logo'>
+				<img src='logo.png'></img>
+			</Link>
+			{/* <div className="text">
         <h1>Hi, I'm Yousif Abozid</h1>
-      </div>
-      <div className={`links ${showMenu ? 'show' : ''}`}>
-        <ul>
-          <li>
-            <Link href="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
-      </div>
-      <div
-        className={`menu-button ${showMenu ? 'show' : ''}`}
-        onClick={toggleMenu}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  )
+      </div> */}
+			<div className={`links ${showMenu ? 'show' : ''}`}>
+				<ul>
+					<li>
+						<Link href='/projects'>Projects</Link>
+					</li>
+					<li>
+						<Link href='/about'>About</Link>
+					</li>
+				</ul>
+			</div>
+			<div
+				className={`menu-button ${showMenu ? 'show' : ''}`}
+				onClick={toggleMenu}>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	)
 }
 
 export default Header
