57c206b7eed3d3ed8a68f7d112159117c8d872f0
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-29 14:12:15 +0200

    Refactor about page CSS styles

--- app/about/page.js
+++ app/about/page.js
@@ -6,94 +6,96 @@ import Image from 'next/image'
  * @return {JSX.Element} A div container with the class name "about_container".
  */
 export default function About() {
-  return (
-    <div className="about_container">
-      <div className="avatar">
-        <Image
-          alt="Yousif Abozid"
-          width={0}
-          height={0}
-          priority={true}
-          src={
-            'https://res.cloudinary.com/octopus-labs/image/upload/v1693155786/yousif-mini.jpg'
-          }></Image>
-      </div>
-      <div className="bio">
-        <p>
-          Hello and welcome to my world of creativity and code! I'm Yousif
-          Abozid, a passionate and dedicated Full Stack Software Engineer,
-          Novelist, and Entrepreneur from the vibrant city of Zagazig, Egypt. As
-          you explore this page, I want to share my journey, my passions, and
-          the unique blend of skills that make me who I am.
-        </p>
-        <p>
-          My coding journey began in 2018, when I took my first steps into the
-          realm of software development. From the very beginning, I was
-          captivated by the endless possibilities that code offers – the power
-          to create, to shape ideas into reality, and to build digital
-          experiences that inspire and delight. With each line of code I write,
-          I embark on a journey of innovation and exploration, constantly
-          pushing the boundaries of my technical prowess.
-        </p>
-        <p>
-          My expertise spans a wide range of technologies, from the fundamental
-          trio of HTML, CSS, and JavaScript to the intricate world of
-          TypeScript, SQL, and Bash scripting. I've dived deep into languages
-          like Python and C, and harnessed the power of frameworks like React.js
-          and Next.js to craft captivating user interfaces that bridge the gap
-          between aesthetics and functionality. Whether it's building responsive
-          websites, dynamic web applications, or seamless RESTful APIs, I thrive
-          on the challenges that the ever-evolving tech landscape presents.
-        </p>
-        <p>
-          My journey isn't limited to just code. As a Novelist, I've found a
-          unique outlet for my creativity. I've penned a novel that's waiting to
-          be shared with the world, weaving stories that resonate with the human
-          experience. I believe that the art of storytelling is a powerful way
-          to connect, inspire, and provoke thought, and I strive to incorporate
-          this perspective into my work as a developer.
-        </p>
-        <p>
-          Entrepreneurship is another facet of my journey. I'm proud to be the
-          Founder & CEO of Gig Bud, a dynamic startup that aims to connect job
-          seekers with companies through an innovative online platform. The
-          challenges of entrepreneurship have sharpened my business acumen and
-          deepened my understanding of innovation and leadership. I'm on a
-          mission to transform ideas into reality and make a meaningful impact
-          on the world.
-        </p>
-        <p>
-          In my quest for knowledge, I've earned credentials from reputable
-          institutions, including Udacity and Helsinki University. Currently,
-          I'm immersing myself in the Software Engineering Program at ALX
-          Africa, where I continue to expand my horizons and collaborate with
-          fellow aspiring engineers.
-        </p>
-        <p>
-          As a lifelong learner, I'm committed to staying at the forefront of
-          technology. From cloud services like AWS and Heroku to version control
-          with Git and GitHub, I'm well-versed in tools that streamline
-          development and deployment. I embrace the challenges of debugging and
-          testing, and I'm proficient in testing frameworks like Jest, Jasmine,
-          and Cypress.
-        </p>
-        <p>
-          My journey is driven by a simple philosophy — to bridge the gap
-          between creativity and technology. I believe that innovation arises
-          from the intersections of diverse disciplines, and I'm passionate
-          about creating experiences that are not just functional, but
-          delightful. With each project I undertake, I aim to elevate the user
-          experience, blend artistry with precision, and craft solutions that
-          leave a lasting impact.
-        </p>
-        <p>
-          Join me on this exciting journey as I explore the ever-changing
-          landscape of technology, literature, and business. Whether it's
-          writing code, weaving stories, or shaping startups, I'm committed to
-          making a difference. Let's collaborate, innovate, and inspire change,
-          one line of code, one chapter, and one venture at a time."
-        </p>
-      </div>
-    </div>
-  )
+	return (
+		<div className='about_container'>
+			<div className='avatar'>
+				<Image
+					alt='Yousif Abozid'
+					width={0}
+					height={0}
+					priority={true}
+					src={
+						'https://res.cloudinary.com/octopus-labs/image/upload/v1693155786/yousif-mini.jpg'
+					}
+				/>
+			</div>
+
+			<div className='bio'>
+				<p>
+					Hello and welcome to my world of creativity and code! I'm Yousif
+					Abozid, a passionate and dedicated Full Stack Software Engineer from
+					the vibrant city of Zagazig, Egypt. As you explore this page, I want
+					to share my journey, my passions, and the unique blend of skills that
+					make me who I am.
+				</p>
+				<p>
+					My coding journey began in 2018, when I took my first steps into the
+					realm of software development. From the very beginning, I was
+					captivated by the endless possibilities that code offers - the power
+					to create, to shape ideas into reality, and to build digital
+					experiences that inspire and delight. With each line of code I write,
+					I embark on a journey of innovation and exploration, constantly
+					pushing the boundaries of my technical prowess.
+				</p>
+				<p>
+					My expertise spans a wide range of technologies, from the fundamental
+					trio of HTML, CSS, and JavaScript to the intricate world of
+					TypeScript, SQL, and Bash scripting. I've dived deep into languages
+					like Python and C, and harnessed the power of frameworks like React.js
+					and Next.js to craft captivating user interfaces that bridge the gap
+					between aesthetics and functionality. Whether it's building responsive
+					websites, dynamic web applications, or seamless RESTful APIs, I thrive
+					on the challenges that the ever-evolving tech landscape presents.
+				</p>
+				{/* <p>
+					My journey isn't limited to just code. As a Novelist, I've found a
+					unique outlet for my creativity. I've penned a novel that's waiting to
+					be shared with the world, weaving stories that resonate with the human
+					experience. I believe that the art of storytelling is a powerful way
+					to connect, inspire, and provoke thought, and I strive to incorporate
+					this perspective into my work as a developer.
+				</p>
+				<p>
+					Entrepreneurship is another facet of my journey. I'm proud to be the
+					Founder & CEO of Gig Bud, a dynamic startup that aims to connect job
+					seekers with companies through an innovative online platform. The
+					challenges of entrepreneurship have sharpened my business acumen and
+					deepened my understanding of innovation and leadership. I'm on a
+					mission to transform ideas into reality and make a meaningful impact
+					on the world.
+				</p> */}
+				<p>
+					In my quest for knowledge, I've earned credentials from reputable
+					institutions, including Udacity and Helsinki University. Currently,
+					I'm immersing myself in the Software Engineering Program at Holberton
+					School & ALX Africa, where I continue to expand my horizons and
+					collaborate with fellow aspiring engineers.
+				</p>
+				<p>
+					As a lifelong learner, I'm committed to staying at the forefront of
+					technology. From cloud services like AWS and Heroku to version control
+					with Git and GitHub, I'm well-versed in tools that streamline
+					development and deployment. I embrace the challenges of debugging and
+					testing, and I'm proficient in testing frameworks like Jest, Jasmine,
+					and Cypress.
+				</p>
+				<p>
+					My journey is driven by a simple philosophy — to bridge the gap
+					between creativity and technology. I believe that innovation arises
+					from the intersections of diverse disciplines, and I'm passionate
+					about creating experiences that are not just functional, but
+					delightful. With each project I undertake, I aim to elevate the user
+					experience, blend artistry with precision, and craft solutions that
+					leave a lasting impact.
+				</p>
+				<p>
+					Join me on this exciting journey as I explore the ever-changing
+					landscape of technology, literature, and business. Whether it's
+					writing code, weaving stories, or shaping startups, I'm committed to
+					making a difference. Let's collaborate, innovate, and inspire change,
+					one line of code at a time."
+				</p>
+			</div>
+		</div>
+	)
 }
