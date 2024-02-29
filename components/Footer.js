095633bf92171709e8ab09da1702ad9e2da261a1
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-29 23:13:34 +0200

    Add more height to footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,31 +8,31 @@ import React from 'react'
  * @return {JSX.Element} The Footer component.
  */
 const Footer = () => {
-  return (
-    <div className="footer">
-      <div className="logo">
-        <img src="coding.svg" alt="Coding" />
-      </div>
-      <div className="middle">
-        <div className="text">
-          Made with ♥️ by <Link href="https://github.com/YousifAbozid">Me</Link>{' '}
-          Source Code on
-        </div>
-        <div className="github">
-          <Link href="https://github.com/YousifAbozid/YousifAbozid.github.io">
-            <img src="github.svg" alt="GitHub" />
-          </Link>
-        </div>
-      </div>
-      <div className="scroll">
-        <img
-          src="up.svg"
-          alt="Go Up"
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-        />
-      </div>
-    </div>
-  )
+	return (
+		<div className='footer'>
+			<div className='logo'>
+				<img src='coding.svg' alt='Coding' />
+			</div>
+			<div className='middle'>
+				<div className='text'>
+					Made with ♥️ by <Link href='https://github.com/YousifAbozid'>Me</Link>{' '}
+					Source Code on
+				</div>
+				<div className='github'>
+					<Link href='https://github.com/YousifAbozid/YousifAbozid.github.io'>
+						<img src='github.svg' alt='GitHub' />
+					</Link>
+				</div>
+			</div>
+			<div className='scroll'>
+				<img
+					src='up.svg'
+					alt='Go Up'
+					onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Footer
